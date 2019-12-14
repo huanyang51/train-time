fa1818fc1aff67cbbf2fc4e367c48c2f3f733579
hycoding51 <yanghuan.bnu@gmail.com>
2019-12-13 22:01:40 -0500

    doc: add initial files with basic styles

---
+++ app.js
@@ -0,0 +1,20 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.6.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDlVZ0vRFo_5lEuuhaftcaBaRwaTXOYTuI",
+    authDomain: "train-time-ea8c5.firebaseapp.com",
+    databaseURL: "https://train-time-ea8c5.firebaseio.com",
+    projectId: "train-time-ea8c5",
+    storageBucket: "train-time-ea8c5.appspot.com",
+    messagingSenderId: "282195264188",
+    appId: "1:282195264188:web:63a552a9fb086186fe46b9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
